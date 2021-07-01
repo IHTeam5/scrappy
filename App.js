ba0ca9194d1c387594fa777d3525ba77b0908be5
Meelod Waheed <meelod.waheed360@gmail.com>
2021-07-01 17:25:58 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,61 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-/*import facility from './components/facility';
-import home from '.components/home';
-import splash from '.components/splash';*/
+import React from 'react'
+import { mapping, light as lightTheme } from '@eva-design/eva'
+import { ApplicationProvider } from 'react-native-ui-kitten'
+import TabNavigator from './navigation/TabNavigator'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const ListHeader = () => {
-  //View to set in Header
-  return (
-    <View style={styles.headerFooterStyle}>
-      <Text style={styles.textStyle}>This is Header</Text>
-    </View>
-  );
-};
-
-const ListFooter = () => {
-  //View to set in Footer
-  return (
-    <View style={styles.headerFooterStyle}>
-      <Text style={styles.textStyle}>This is Footer</Text>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  emptyListStyle: {
-    padding: 10,
-    fontSize: 18,
-    textAlign: 'center',
-  },
-  itemStyle: {
-    padding: 10,
-  },
-  headerFooterStyle: {
-    width: '100%',
-    height: 45,
-    backgroundColor: '#606070',
-  },
-  textStyle: {
-    textAlign: 'center',
-    color: '#fff',
-    fontSize: 18,
-    padding: 7,
-  },
-});
+const App = () => (
+  <ApplicationProvider mapping={mapping} theme={lightTheme}>
+    <TabNavigator />
+  </ApplicationProvider>
+)
+export default App
