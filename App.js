9fade455d0b66000aed637e337f7593469ee9215
Meelod Waheed <meelod.waheed360@gmail.com>
2021-06-30 23:35:28 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+/*import facility from './components/facility';
+import home from '.components/home';
+import splash from '.components/splash';*/
 
 export default function App() {
   return (
@@ -11,11 +14,48 @@ export default function App() {
   );
 }
 
+const ListHeader = () => {
+  //View to set in Header
+  return (
+    <View style={styles.headerFooterStyle}>
+      <Text style={styles.textStyle}>This is Header</Text>
+    </View>
+  );
+};
+
+const ListFooter = () => {
+  //View to set in Footer
+  return (
+    <View style={styles.headerFooterStyle}>
+      <Text style={styles.textStyle}>This is Footer</Text>
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  emptyListStyle: {
+    padding: 10,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  itemStyle: {
+    padding: 10,
+  },
+  headerFooterStyle: {
+    width: '100%',
+    height: 45,
+    backgroundColor: '#606070',
+  },
+  textStyle: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 18,
+    padding: 7,
   },
 });
---
+++ components/facility.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+/* import firebase from ./database/firebase  (we need to implement this later)*/
+
+const facility = props => {
+    
+}
